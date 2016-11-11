5d4f94baddfc59972229646f0a8de2746134ad55
Pau iglesias <14270729b@iespoblenou.org>
2016-11-11 15:30:06 +0100

    Llibreries i ContentProvider Afegit.

---
+++ app/src/main/java/ContentProvider/MagicContentProvider.java
@@ -0,0 +1,24 @@
+package ContentProvider;
+
+import Pojos.Card;
+import nl.littlerobots.cupboard.tools.BuildConfig;
+import nl.littlerobots.cupboard.tools.provider.CupboardContentProvider;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+/**
+ * Created by 14270729b on 11/11/16.
+ */
+
+public class MagicContentProvider extends CupboardContentProvider {
+
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID+".provider";
+
+    static {
+        cupboard().register(Card.class);
+    }
+
+    public MagicContentProvider(){
+        super(AUTHORITY,1);
+    }
+}
